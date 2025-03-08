a203f5f61e73f905250531404b81ff9c0f59ab57
MateusClepf <mateusclepf@gmail.com>
2025-03-08 10:05:22 +0100

    Chore: removing unecessary variables.

--- zone-modules/bot-management/main.tf
+++ zone-modules/bot-management/main.tf
@@ -1,8 +1,8 @@
 # main.tf
 resource "cloudflare_bot_management" "this" {
   zone_id                = var.zone_id
-  ai_bots_protection     = var.ai_bots_protection
-  auto_update_model      = var.auto_update_model
-  enable_js              = var.enable_js
-  suppress_session_score = var.suppress_session_score
+  ai_bots_protection     = "disabled"
+  auto_update_model      = true
+  enable_js              = true
+  suppress_session_score = false
 }
